5f0d82da590e05dd43acda9eb3041de9c428daf6
Сергей <95445727+RePti-LoiD@users.noreply.github.com>
2023-03-24 10:03:25 +0300

    settings integration

---
+++ Data/SettingsSaveHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace CardLearnApp.Data
+{
+    internal class SettingsSaveHandler
+    {
+        private static string directory = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+        public static string JsonDataPath = Path.Combine(directory, "Bundles.json");
+
+
+    }
+}
